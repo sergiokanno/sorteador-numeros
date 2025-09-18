d9588890a5d68be6c16c3f66dd60996c05f8fc70
Windows (Sergio Kanno) <sergio.kanno@outlook.com.br>
2025-09-18 07:37:11 -0300

    Implementando proteção na entrada de valores da função sortear

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 function sortear() {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
 
     alert(`Quantidade de números: ${quantidade}`);
     alert(`Do número: ${de}`);
