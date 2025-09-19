80c44f14f3bf6656bb8e6f54c553b154c2154903
Windows (Sergio Kanno) <sergio.kanno@outlook.com.br>
2025-09-18 23:05:27 -0300

    Exibindo na tela os números sorteados

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ function sortear() {
         sorteados.push(numero);
     }
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
 }
 
 function obterNumeroAleatorio(min, max) {
