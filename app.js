bc0f68f5e253123693e8f3fddc8bc33e821e81a6
Windows (Sergio Kanno) <sergio.kanno@outlook.com.br>
2025-09-18 23:54:14 -0300

    Implementando Botão de reiniciar

--- app.js
+++ app.js
@@ -28,3 +28,25 @@ function sortear() {
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function reiniciar() {
+        document.getElementById('quantidade').value = '';
+        document.getElementById('de').value = '';
+        document.getElementById('ate').value = '';
+        document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+        alterarStatusBotao();
+}
+
+function alterarStatusBotao() {
+        let botao = document.getElementById('btn-reiniciar');
+
+        if (botao.classList.contains('container__botao-desabilitado')) {
+                botao.classList.remove('container__botao-desabilitado');
+                botao.classList.add('container__botao');
+        } else {
+                botao.classList.remove('container__botao');
+                botao.classList.add('container__botao-desabilitado');
+        }
+
+        alert('Os campos foram limpos. Você já pode fazer um novo sorteio!');
+}
